5733fbcf1a9c2dcfb91006e6db2b0d7fdcae20ed
Arthur Geel <geelaj01@gmail.com>
2022-12-09 15:08:32 +0100

    Fine tune some visuals

--- scenes/PlayerDeath.gd
+++ scenes/PlayerDeath.gd
@@ -3,10 +3,14 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var gravity = 1000
 
+var words = ["Oof", "Ow", "Ouch", "Argh", "Darn", "Youch", "Yoink", "Owie", "Shucks", "Crunch", "Yeet", "Ugh", "Golly", "Oy"]
+
 func _ready():
 	if (velocity.x > 0):
 		$Visuals.scale = Vector2(-1, 1)
 	$HurtboxArea.connect("area_entered", self, "on_hurtbox_entered")
+	# var randomWord = words[randi() % words.size()]
+	# $Text/Label.text = randomWord
 
 func _process(delta):
 	velocity.y += gravity * delta
---
+++ scenes/Scroll.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+export(String, MULTILINE) var text
+
+var lastAnimation = null
+
+func _ready():
+	# Update label text
+	$Node2D/PanelContainer/MarginContainer/Label.text = text
+	$Area2D.connect("area_entered", self, "on_area_entered")
+	$Area2D.connect("area_exited", self, "on_area_exited")
+	$AnimationPlayer.playback_speed = 1.25
+
+func on_area_entered(_area2d):
+	$AnimationPlayer.current_animation = "opened"
+
+func on_area_exited(_area2d):
+	$AnimationPlayer.play_backwards("opened")
+	$AnimationPlayer.playback_speed = 2
+	$AnimationPlayer.queue("idle")
