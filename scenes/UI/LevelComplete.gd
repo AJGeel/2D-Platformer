53715492585300ddfd832d35b564e9c7785e2925
Arthur Geel <geelaj01@gmail.com>
2022-12-09 16:21:36 +0100

    Update levelComplete UI

--- scenes/UI/LevelComplete.gd
+++ scenes/UI/LevelComplete.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+onready var nextButton = $CanvasLayer/CenterContainer/VBoxContainer/NextButton
+onready var retryButton = $CanvasLayer/CenterContainer/VBoxContainer/RetryButton
+
 func _ready():
-	$MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_next_button_pressed")
+	nextButton.connect("pressed", self, "on_next_button_pressed")
+	retryButton.connect("pressed", self, "on_retry_button_pressed")
+	nextButton.grab_focus()
 
 func on_next_button_pressed():
 	$"/root/LevelManager".increment_level()
+
+func on_retry_button_pressed():
+	print("Retry")
+	pass
