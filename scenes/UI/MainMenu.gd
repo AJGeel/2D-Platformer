b1f2a5efe0d6e70ec87085cc086feaedf40cc42e
Arthur Geel <geelaj01@gmail.com>
2022-12-19 20:43:12 +0100

    Add feedback button

--- scenes/UI/MainMenu.gd
+++ scenes/UI/MainMenu.gd
@@ -4,11 +4,13 @@ func _ready():
 	var playButton = $MainMenuOptions/PlayButton
 	var optionsButton = $MainMenuOptions/OptionsButton
 	var connectButton = $MainMenuOptions/ConnectButton
+	var feedbackButton = $MainMenuOptions/FeedbackButton
 	var quitButton = $MainMenuOptions/QuitButton
 	
 	playButton.connect("pressed", self, "on_play_pressed")
 	optionsButton.connect("pressed", self, "on_options_pressed")
 	connectButton.connect("pressed", self, "on_connect_pressed")
+	feedbackButton.connect("pressed", self, "on_feedback_pressed")
 	quitButton.connect("pressed", self, "on_quit_pressed")
 	
 	playButton.grab_focus()
@@ -16,11 +18,15 @@ func _ready():
 func on_play_pressed():
 	$"/root/LevelManager".change_level(0)
 
-func on_quit_pressed():
-	get_tree().quit()
-
 func on_options_pressed():
 	$"/root/ScreenTransitionManager".transition_to_scene("res://scenes/UI/OptionsMenuStandalone.tscn")
 
 func on_connect_pressed():
 	$"/root/ScreenTransitionManager".transition_to_scene("res://scenes/UI/ConnectMenu.tscn")
+
+func on_feedback_pressed():
+	print("Feedback btn pressed")
+	OS.shell_open("https://discord.gg/mTPmh74byM")
+
+func on_quit_pressed():
+	get_tree().quit()
