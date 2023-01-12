0acae6f331e141ac7daf5f30baf0a59f84f4bd37
ajgeel <geelaj01@gmail.com>
2023-01-12 08:49:48 +0100

    Fix interactionComponent

--- scenes/Interactions/CellGate/CellGate.gd
+++ scenes/Interactions/CellGate/CellGate.gd
@@ -3,36 +3,38 @@ extends Node2D
 export(int, 2, 4) var GATE_HEIGHT
 export(bool) var isOpen = false
 
+var isOpening = false
+
 var SPRITE_2H = preload("res://assets/interactions/cell-gate/cell-gate-2h.png")
 var SPRITE_3H = preload("res://assets/interactions/cell-gate/cell-gate-3h.png")
 var SPRITE_4H = preload("res://assets/interactions/cell-gate/cell-gate-4h.png")
 
 const FACTOR = 18
 
-# Update sprite and collision props on loaded
+# Update sprite and collision props on init
 func _ready():
 	update_sprite()
 	update_collision_props()
-	# TODO: Update Lock Height
+	# TODO?: Update Lock Height
 
-#func interaction_can_interact(interactionComponentParent : Node) -> bool:
-	# return interactionComponentParent is Player
-	#print(interactionComponentParent)
-	#return true
+func interaction_can_interact() -> bool:
+	return true
 
-func interaction_interact(interactionComponentParent : Node) -> void:
-	if isOpen:
+func interaction_interact(interactionTarget: Node) -> void:
+	if (isOpen || isOpening):
 		return
 	
-	# if player has key:
-	# $AnimationPlayer.play("SwingOpen")
-	# $Lock/AnimationPlayer.play("Explode")
+	# if player holds one or more keys:
+	#$AnimationPlayer.play("SwingOpen")
+	#$Lock/AnimationPlayer.play("Explode")
 	
 	# else: 
 	# $Lock/AnimationPlayer.play("Jiggle")
 	
+	isOpening = true
 	$AnimationPlayer.play("SwingOpen")
 	$Lock/AnimationPlayer.play("Explode")
+	isOpen = true
 
 # Update the gate's sprite to match the set gate height
 func update_sprite():
@@ -52,8 +54,16 @@ func update_collision_props():
 	var collisionNode = $StaticBody2D/CollisionShape2D
 	var dustParticlesNode = $DustParticles
 	
+	print("Updating collision props for ", self)
+	print("Original state:")
+	print(collisionNode.shape.extents.y)
+	print(collisionNode.position.y)
 	collisionNode.shape.extents.y = FACTOR * (GATE_HEIGHT / 2) # (18, 27, 36)
 	collisionNode.position.y = -1 * FACTOR * ((GATE_HEIGHT - 1) / 2) # (0, -9, -18)
+	
+	print("Final state:")
+	print(collisionNode.shape.extents.y)
+	print(collisionNode.position.y)
 	
 	dustParticlesNode.amount = 4 * GATE_HEIGHT # (8, 12, 16)
 	dustParticlesNode.emission_rect_extents.y = GATE_HEIGHT * 8 - 4 # (12, 20, 28)
--- scenes/Interactions/InteractionComponent.gd
+++ scenes/Interactions/InteractionComponent.gd
@@ -8,31 +8,29 @@ var interactionTarget : Node
 
 func _process(_delta):
 	# Check whether the player is trying to interact
-	if (interactionTarget != null && Input.is_action_just_pressed("ui_accept")):
-		# If so, call interaction_interact() if the target supports it
-		if (interactionTarget.has_method("interaction_interact")):
+	if (Input.is_action_just_pressed("ui_accept")):
+		print("Player tried to interact")
+		if (interactionTarget != null):
 			interactionTarget.interaction_interact(self)
 
 # Signal triggered when the player enters an interactable area
-func _on_InteractionComponent_area_entered(body):
+func _on_InteractionComponent_area_entered(area):
+	var canInteract: bool = false
 	
-	print("Entered interaction component")
-	
-	var canInteract := false
-	
-	if (body.has_method("interaction_can_interact")):
-		canInteract = body.interaction_can_interact(get_node(INTERACTION_PARENT))
-	
-	if not canInteract:
+	# Note: Interactable objects should always be structured like:
+	# > Object
+	# > > InteractArea
+	if (area.get_parent().has_method("interaction_can_interact")):
+		canInteract = true
+	else:
 		return
 	
 	# Store the thing we'll be interacting with, so we can trigger it from _process
-	interactionTarget = body
+	interactionTarget = area.get_parent()
 	emit_signal("on_interactable_changed", interactionTarget)
 
 # Signal triggered when the player exits an interactable area
-func _on_InteractionComponent_area_exited(body):
-	print("Exited interaction component")
-	if (body == interactionTarget):
+func _on_InteractionComponent_area_exited(area):
+	if (area.get_parent() == interactionTarget):
 		interactionTarget = null
 		emit_signal("on_interactable_changed", null)
